79e9a048e58fda240dfc4c8d4ef8334b80ef4572
NIHAT <NIHAT@DESKTOP-3F2LN5P>
2024-08-15 01:21:16 +0300

    Add project files.

---
+++ Abstraction/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Abstraction
+{
+    abstract class Employee // Abstract sınıfı
+    {
+        private string _name; // _name field
+        private string _surName; // _surName field
+        private string _departman; // _departman field
+        public string Name // Name property
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string SurName // SurName property
+        {
+            get
+            {
+                return _surName;
+            }
+            set
+            {
+                _surName = value;
+            }
+        }
+
+        public string Departman // Departman property
+        {
+            get
+            {
+                return _departman;
+            }
+            set
+            {
+                _departman = value;
+            }
+        }
+
+        public abstract void Mission(); // Abstract metodu
+
+    }
+
+    class Profession : Employee // Çalışanın mesleğini gireceği class
+    {
+
+        public override void Mission()
+        {
+
+            Departman = Console.ReadLine() ?? ""; // Nesnenin bilgilerini konsoldan giriyoruz.
+            Console.WriteLine($"{Name} {SurName} {Departman} olarak çalışıyorsunuz.");
+        }
+    }
+
+
+
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee1 = new Profession(); // Employee sınıfının türünden Profession nesnesi oluşturuyoruz.
+
+            employee1.Name = "Ali"; // Nesnenin bilgilerini giriyoruz.
+
+            employee1.SurName = "Güneş"; // Nesnenin bilgilerini giriyoruz.
+
+            Console.WriteLine($"{employee1.Name} yaptığınız işi giriniz.");
+
+            employee1.Mission(); // Nesnenin çalıştığı pozisyon bilgisi metot içerisinde girilecek ve ekranda yazdırılacak.
+
+
+            // ------------------------------------------------------------------------
+
+            Employee employee2 = new Profession();
+
+            Console.WriteLine("\nAdınızı giriniz...");
+
+            employee2.Name = Console.ReadLine() ?? ""; // Nesnenin bilgilerini konsoldan giriyoruz.
+
+            Console.WriteLine("Soyadınızı giriniz...");
+
+            employee2.SurName = Console.ReadLine() ?? ""; // Nesnenin bilgilerini konsoldan giriyoruz.
+
+            Console.WriteLine($"{employee2.Name} yaptığınız işi giriniz.");
+
+            employee2.Mission(); // Nesnenin çalıştığı pozisyon bilgisi metot içerisinde girilecek ve ekranda yazdırılacak.
+
+        }
+    }
+}
